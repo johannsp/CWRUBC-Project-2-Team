1e2440a5839469fad61b6cb0af8d9bf2017e55f0
John L Spetz <JohnLSpetz@zoho.com>
2020-08-31 16:37:10 -0400

    Upgrade index.js to ES6 const usage

--- models/index.js
+++ models/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var fs        = require('fs');
-var path      = require('path');
-var Sequelize = require('sequelize');
-var basename  = path.basename(module.filename);
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
-var db        = {};
+const fs        = require('fs');
+const path      = require('path');
+const Sequelize = require('sequelize');
+const basename  = path.basename(module.filename);
+const env       = process.env.NODE_ENV || 'development';
+const config    = require(__dirname + '/../config/config.json')[env];
+const db        = {};
 
 // If password was left as null in ../config/config.json file
 // and if file .mysql is present in application root directory
@@ -17,11 +17,20 @@ if (config.password === null && fs.existsSync(".mysql")) {
   config.password = String(fs.readFileSync(".mysql")).trim();
 }
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+/* {{{ **
+** if (config.use_env_variable) {
+**   var sequelize = new Sequelize(process.env[config.use_env_variable]);
+** } else {
+**   var sequelize = new Sequelize(config.database, config.username, config.password, config);
+** }
+** }}} */
+// When defined as const a variable can only be set once and ES6 const and let
+// limit scope to the code block where a variable is defined.  Use a ternary
+// operator to absorb the IF statement so only one assignment at outside scope
+// level is needed.
+const sequelize = (config.use_env_variable)
+  ? new Sequelize(process.env[config.use_env_variable])
+  : new Sequelize(config.database, config.username, config.password, config);
 
 fs
   .readdirSync(__dirname)
@@ -29,7 +38,7 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(function(file) {
-    var model = sequelize['import'](path.join(__dirname, file));
+    const model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
   });
 
