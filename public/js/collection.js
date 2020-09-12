2c5f95a32c6b4c942717ca71639475ccb8550f77
John L Spetz <JohnLSpetz@zoho.com>
2020-09-12 06:36:39 -0400

    Code minimal implementation of collection display

--- public/js/collection.js
+++ public/js/collection.js
@@ -5,17 +5,39 @@ $(document).ready(() => {
 
   //Function to display music collection as cards
   function myCollection() {
-    //This for loop is a placeholder. It may need to be a for each?
-    for (let i = 0; i < 9999; i++) {
-      const collectionCards = `<div class="card text-center m-5" style="width: 14rem;">
-  <p class="card-header text-center">${ALBUM} by ${ARTIST}</p>
+    $.ajax({
+      url: "/api/albums",
+      method: "GET"
+    }).then(response => {
+      response.forEach(item => {
+        console.log("∞° item=\n" + JSON.stringify(item));
+        const collectionCards = `<div class="card text-center m-5" style="width: 14rem;">
+  <p class="card-header text-center">${item.title} by ${item.artist}</p>
   <div class="card-body">
     <div class="justify-content-center"">
-      <img src="${ARTWORKLINK}"> 
+      <img src="${item.artLink}"> 
     </div>
   </div>
 </div>`;
-      $("#collectionDisplay").append(collectionCards);
-    }
+        $("#collectionDisplay").append(collectionCards);
+      });
+      $.ajax({
+        url: "/api/songs",
+        method: "GET"
+      }).then(response => {
+        response.forEach(item => {
+          console.log("∞° item=\n" + JSON.stringify(item));
+          const collectionCards = `<div class="card text-center m-5" style="width: 14rem;">
+  <p class="card-header text-center">${item.title} by ${item.artist}</p>
+  <div class="card-body">
+    <div class="justify-content-center"">
+      <img src="${item.artLink}"> 
+    </div>
+  </div>
+</div>`;
+          $("#collectionDisplay").append(collectionCards);
+        });
+      });
+    });
   }
 });
