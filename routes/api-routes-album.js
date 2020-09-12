4335b66efdc44d57ed633f037a1b8a77a894a15f
John L Spetz <JohnLSpetz@zoho.com>
2020-09-12 00:48:48 -0400

    Fixes for ESLint and improve status logging

--- routes/api-routes-album.js
+++ routes/api-routes-album.js
@@ -1,46 +1,42 @@
 // Requiring our models as we've configured it
 const db = require("../models");
 
-
 module.exports = function(app) {
   // Find all albums and return them to the user with res.json
-  app.get("/api/albums", function(req, res) {
-      ///include: [db.Post] if doing a left join
-    db.Album.findAll({}).then(function(dbAlbum){
+  app.get("/api/albums", (req, res) => {
+    ///include: [db.Post] if doing a left join
+    db.Album.findAll({}).then(dbAlbum => {
       res.json(dbAlbum);
     });
   });
 
-  app.get("/api/albums/:id", function(req, res) {
+  app.get("/api/albums/:id", (req, res) => {
     // Find one Album with the id in req.params.id and return them to the user with res.json
     db.Album.findOne({
       where: {
         id: req.params.id
       }
-    }).then(function(dbAlbum) {
+    }).then(dbAlbum => {
       res.json(dbAlbum);
     });
   });
 
-
-  app.post("/api/albums", function(req, res) {
-  //Create an Album with the data available to us in req.body
-    console.log(req.body);
-    db.Album.create(req.body).then(function(dbAlbum) {
+  app.post("/api/albums", (req, res) => {
+    // Create an Album with the data available to us in req.body
+    console.log(`POST: /api/albums req.body=${req.body}`);
+    db.Album.create(req.body).then(dbAlbum => {
       res.json(dbAlbum);
     });
   });
 
-  
-  app.delete("/api/albums/:id", function(req, res) {
+  app.delete("/api/albums/:id", (req, res) => {
     // Delete the Album with the id available to us in req.params.id
     db.Album.destroy({
       where: {
         id: req.params.id
       }
-    }).then(function(dbAlbum) {
+    }).then(dbAlbum => {
       res.json(dbAlbum);
     });
   });
-
 };
